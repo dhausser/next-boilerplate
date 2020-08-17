7e105b61823d8688bf9a7419a5ab6217803b9a06
Davy Hausser <davy.hausser@gmail.com>
2020-08-17 15:44:34 +0200

    :sparkles: support of api routes added with SWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
-import styles from "styles/Home.module.css";
+import useSWR from "swr";
 import { Button } from "@/components/button";
+import styles from "styles/Home.module.css";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 function HomePage() {
+  const { data, error } = useSWR("/api/user", fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
   return (
     <div className={styles.container}>
       <div className={styles.main}>
-        Welcome to Next.js!
+        Welcome {data.name}
         <Button />
       </div>
     </div>
