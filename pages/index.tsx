9f45d091c69e015f59a017313af8b3f0be5ad8e3
Davy Hausser <davy.hausser@gmail.com>
2020-08-18 09:42:35 +0200

    :recycle: adding eslint with prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,23 @@
-import useSWR from 'swr';
-import { Button } from '@/components/button';
-import styles from 'styles/Home.module.css';
+import useSWR from 'swr'
+import { Button } from '@/components/button'
+import styles from 'styles/Home.module.css'
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 function HomePage() {
-  const { data, error } = useSWR('/api/user', fetcher);
+  const { data, error } = useSWR('/api/user', fetcher)
 
-  if (error) return <div>Failed to load</div>;
-  if (!data) return <div>Loading...</div>;
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
 
   return (
     <div className={styles.container}>
       <div className={styles.main}>
-        Welcome
-        {' '}
-        {data.name}
+        Welcome {data.name}
         <Button />
       </div>
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
