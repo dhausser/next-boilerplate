8679b4247dd6ef53c8c0cb3c2915f7a672986cc9
Davy Hausser <davy.hausser@gmail.com>
2020-08-19 08:52:29 +0200

    :sparkles: prisma integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import useSWR from 'swr'
+import { Post, PrismaClient } from '@prisma/client'
+import { GetStaticProps } from 'next'
 import { Button } from '@/components/button'
 import styles from 'styles/Home.module.css'
 
+interface Props {
+  posts: Post[]
+}
+
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
-function HomePage() {
+function HomePage({ posts }: Props) {
   const { data, error } = useSWR('/api/user', fetcher)
 
   if (error) return <div>Failed to load</div>
@@ -15,9 +21,28 @@ function HomePage() {
       <div className={styles.main}>
         Welcome {data.name}
         <Button />
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
       </div>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prisma = new PrismaClient()
+
+  const posts = await prisma.post.findMany({
+    select: { id: true, title: true, content: true },
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
 
 export default HomePage
