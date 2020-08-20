c92b4852e7a70bcf0fb73eeb0e3761a44a291bfa
Davy Hausser <davy.hausser@gmail.com>
2020-08-20 15:41:47 +0200

    :recycle: fixed schema with direct declaration of prisma

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -25,26 +25,29 @@ on.start(() => {
   })
 })
 
+const prisma = new Prisma.PrismaClient()
+
 schema.objectType({
   name: 'Post',
   definition(t) {
     t.id('id')
     t.string('title')
     t.string('content', { nullable: true })
-    // t.string('authorId')
-    // t.field('author', {
-    //   type: 'User',
-    //   nullable: false,
-    //   async resolve(root, _args, ctx) {
-    //     const author = await ctx.db.user.findOne({
-    //       where: { id: Number(root.authorId) },
-    //     })
-    //     if (!author) {
-    //       throw new Error(`Cannot find author with id: ${root.authorId}`)
-    //     }
-    //     return author
-    //   },
-    // })
+    t.string('authorId')
+    t.field('author', {
+      type: 'User',
+      nullable: false,
+      async resolve(root, _args, ctx) {
+        const author = await ctx.db.user.findOne({
+          where: { id: Number(root.authorId) },
+        })
+        if (!author) {
+          throw new Error(`Cannot find author with id: ${root.authorId}`)
+        }
+        return author
+      },
+    })
+    t.boolean('published')
   },
 })
 
@@ -54,26 +57,26 @@ schema.objectType({
     t.id('id')
     t.string('email')
     t.string('name')
-    // t.list.field('posts', {
-    //   type: 'Post',
-    //   resolve(root, _args, ctx) {
-    //     return ctx.db.post.findMany({
-    //       where: { authorId: Number(root.id) },
-    //     })
-    //   },
-    // })
-    // t.field('profile', {
-    //   type: 'Profile',
-    //   async resolve(root, _args, ctx) {
-    //     const profile = await ctx.db.profile.findOne({
-    //       where: { userId: Number(root.id) },
-    //     })
-    //     if (!profile) {
-    //       throw new Error(`Cannot find profile for id: ${root.id}`)
-    //     }
-    //     return profile
-    //   },
-    // })
+    t.list.field('posts', {
+      type: 'Post',
+      resolve(root, _args, ctx) {
+        return ctx.db.post.findMany({
+          where: { authorId: Number(root.id) },
+        })
+      },
+    })
+    t.field('profile', {
+      type: 'Profile',
+      async resolve(root, _args, ctx) {
+        const profile = await ctx.db.profile.findOne({
+          where: { userId: Number(root.id) },
+        })
+        if (!profile) {
+          throw new Error(`Cannot find profile for id: ${root.id}`)
+        }
+        return profile
+      },
+    })
   },
 })
 
@@ -83,18 +86,18 @@ schema.objectType({
     t.id('id')
     t.string('bio')
     t.string('userId')
-    // t.field('user', {
-    //   type: 'User',
-    //   async resolve(root, _args, ctx) {
-    //     const user = await ctx.db.user.findOne({
-    //       where: { id: Number(root.userId) },
-    //     })
-    //     if (!user) {
-    //       throw new Error(`Cannot find user for id: ${root.userId}`)
-    //     }
-    //     return user
-    //   },
-    // })
+    t.field('user', {
+      type: 'User',
+      async resolve(root, _args, ctx) {
+        const user = await ctx.db.user.findOne({
+          where: { id: Number(root.userId) },
+        })
+        if (!user) {
+          throw new Error(`Cannot find user for id: ${root.userId}`)
+        }
+        return user
+      },
+    })
   },
 })
 
@@ -103,8 +106,8 @@ schema.queryType({
     t.list.field('posts', {
       type: 'Post',
       nullable: false,
-      async resolve(_root, _args, ctx) {
-        return ctx.db.post.findMany()
+      resolve(_root, _args, _ctx) {
+        return prisma.post.findMany()
       },
     })
   },
